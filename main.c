bcda69ba85700a37fa6fcff87a92e4cb035f0598
lucas8 <luc.linux@mailoo.org>
2017-03-29 10:29:59 +0200

    Add connection from thread to main

--- main.c
+++ main.c
@@ -5,6 +5,11 @@
 #include <cthreads.h>
 #include <mach.h>
 
+struct my_thread {
+    mach_port_t rcv;
+    thread_t thread;
+};
+
 struct integer_message {
     mach_msg_header_t head;
     mach_msg_type_t type;
@@ -20,11 +25,8 @@ int32_t receive_integer(mach_port_t port) {
             0, msg.head.msgh_size, port,
             MACH_MSG_TIMEOUT_NONE, MACH_PORT_NULL);
     if(err != MACH_MSG_SUCCESS) {
-        fprintf(stderr, "Couldn't receive\n");
         exit(err);
     }
-    printf("Received %d\n", msg.inline_integer);
-    fflush(stdout);
     return msg.inline_integer;
 }
 
@@ -51,34 +53,116 @@ void send_integer(mach_port_t port, int32_t integer) {
             msg.head.msgh_size, 0, MACH_PORT_NULL,
             MACH_MSG_TIMEOUT_NONE, MACH_PORT_NULL);
     if(err != MACH_MSG_SUCCESS) {
-        fprintf(stderr, "Couldn't send %d\n", integer);
         exit(err);
     }
-    printf("Queued %d\n", integer);
-    fflush(stdout);
+}
+
+void thread_start(void(*routine)(mach_port_t,void*), mach_port_t *port, void *arg) {
+    (*routine)(*port, arg);
+    /* Thread must terminate itself
+     * It can't return, because the return address
+     * is set to 0.
+     */
+    thread_terminate(mach_thread_self());
+}
+
+/* Only works on x86 32-bit */
+struct my_thread thread_fork(void(*routine)(mach_port_t,void*), void* arg) {
+    vm_size_t stack_size = vm_page_size * 16;
+    kern_return_t ret;
+    thread_t thread;
+    struct my_thread thr;
+    mach_port_t port;
+
+    ret = thread_create(mach_task_self(), &thread);
+    if(ret != KERN_SUCCESS) {
+        perror("thread_create");
+        exit(1);
+    }
+    thr.thread = thread;
+
+    ret = mach_port_allocate(mach_task_self(), MACH_PORT_RIGHT_RECEIVE, &port);
+    if(ret != KERN_SUCCESS) {
+        perror("mach_port_allocate");
+        exit(1);
+    }
+    thr.rcv = port;
+
+    /* Setting the state of the thread */
+    struct i386_thread_state state;
+    unsigned int count;
+
+    count = i386_THREAD_STATE_COUNT;
+    ret = thread_get_state(thread, i386_REGS_SEGS_STATE,
+            (thread_state_t) &state, &count);
+    if(ret != KERN_SUCCESS) {
+        perror("thread_get_state");
+        exit(1);
+    }
+
+    /* Create stack */
+    vm_address_t stack;
+    ret = vm_allocate(mach_task_self(), &stack, stack_size, TRUE);
+    if(ret != KERN_SUCCESS) {
+        perror("vm_allocate");
+        exit(1);
+    }
+    /* Protect the lowest page against access to detect stack overflow */
+    ret = vm_protect(mach_task_self(),
+            stack,                /* The address */
+            vm_page_size,         /* The size */
+            FALSE,                /* Set the current permissions, not the maximum
+                                     permissions allowed */
+            VM_PROT_NONE);        /* Give no permission */
+    if(ret != KERN_SUCCESS) {
+        perror("vm_protect");
+        exit(1);
+    }
+    /* Setup the stack */
+    int* top   = (int*)(stack + stack_size) - 4;
+    *top       = 0;             /* The return address */
+    *(top + 1) = (int) routine; /* The function to launch */
+    *(top + 2) = (int) &port;   /* The port */
+    *(top + 3) = (int) arg;     /* The argument */
+
+    state.eip  = (int) thread_start; /* Start on routine */
+    state.uesp = (int) top;          /* Set stack pointer */
+    state.ebp  = 0;                  /* Clear frame pointer*/
+
+    ret = thread_set_state(thread, i386_REGS_SEGS_STATE,
+            (thread_state_t) &state, i386_THREAD_STATE_COUNT);
+    if(ret != KERN_SUCCESS) {
+        perror("thread_set_state");
+        exit(1);
+    }
+    ret = thread_resume(thread);
+    if(ret != KERN_SUCCESS) {
+        perror("thread_resume");
+        exit(1);
+    }
+
+    return thr;
+}
+
+void thread_kill(struct my_thread* thread) {
+    thread_terminate(thread->thread);
+    mach_port_deallocate(mach_task_self(), thread->rcv);
+}
+
+void routine(mach_port_t port, void* buf) {
+    sleep(1);
+    send_integer(port, *(int*)buf);
 }
 
 int main(int argc, char *argv[]) {
     if(argc && argv) { }
+    struct my_thread thr;
+    int x = 42;
 
-    mach_port_t rcv_port;
-    kern_return_t err;
-
-    err = mach_port_allocate(mach_task_self(),
-            MACH_PORT_RIGHT_RECEIVE,
-            &rcv_port);
-    if(err != KERN_SUCCESS) {
-        perror("error : could not allocate any port\n");
-        exit(err);
-    }
-
-    int r, s;
-    s = 42;
-    send_integer(rcv_port, s);
-    r = receive_integer(rcv_port);
-    printf("r = %d\n", r);
-
-    mach_port_deallocate(mach_task_self(), rcv_port);
+    thr = thread_fork(routine, &x);
+    int r = receive_integer(thr.rcv);
+    printf("Received %d\n", r);
+    thread_kill(&thr);
 
     return 0;
 }
